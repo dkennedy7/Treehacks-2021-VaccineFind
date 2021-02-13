a5feeda9e6634ea9e7920870a7676e13630542f7
Lisa Sam Wang <sam_wang_lisa1121@berkeley.edu>
2021-02-13 13:25:56 -0800

    Initial Commit

---
+++ Vaccine-Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vaccine-Finder
+//
+//  Created by Lisa Sam Wang on 2/13/21.
+//  Copyright © 2021 Lisa Sam Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
